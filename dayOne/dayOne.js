80bf488e6c32f147542cd75fb6836a61722a51f0
riecz <rclemen2@volvocars.com>
2024-12-10 13:39:41 +0100

    day one part two completed

--- dayOne/dayOne.js
+++ dayOne/dayOne.js
@@ -6,8 +6,9 @@ let listRight = []
 let sums = []
 let answer
 
+ 
 const file = readline.createInterface({
-  input: fs.createReadStream('./dayone-input.txt'),
+  input: fs.createReadStream('dayone-input.txt'),
   output: process.stdout,
   terminal: false
 })
@@ -24,16 +25,28 @@ file.on('line', (line) =>{
 file.on('close', () => {
   listLeft.sort()
   listRight.sort()
-
+  
+  // Part One
   for (let i = 0; i < listLeft.length; i++) {
     let diff;
     diff = listLeft[i] - listRight[i]
     if (diff < 0) {
         diff = -diff
     }
-    console.log(diff)
     sums.push(diff)  
   }
   answer = sums.reduce((a, b) => a + b, 0)
-  console.log(answer)
+  console.log(`Answer for part One:  ${answer}`)
+
+  // Part Two
+  sums.length = 0
+  for (let i= 0; i < listLeft.length; i++){
+    if (listRight.includes(listLeft[i])){
+      const count = listRight.filter(num => num == listLeft[i]).length 
+      sums.push(listLeft[i]*count)
+    }
+  }
+  answer = sums.reduce((a, b) => a + b, 0)
+  console.log(`Answer for part Two:  ${answer}`)
+
 })
